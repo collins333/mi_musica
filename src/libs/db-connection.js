2799ed393157ffd0b3ca20d6f3f4367795f3ee12
collins7 <collins.mdfp@gmail.com>
2022-04-05 17:02:04 +0200

    probando la conexion

--- src/libs/db-connection.js
+++ src/libs/db-connection.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
 
-let db = mongoose.connect('mongodb://collins:collins@cluster0.jncqe.mongodb.net/test', {
+let db = mongoose.connect('mongodb+srv://collins:collins@cluster0.jncqe.mongodb.net/musica?retryWrites=true&w=majority', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
 })
+// let db = mongoose.connect('mongodb://localhost:27017/musica', {
+//   useCreateIndex: true,
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true
+// })
   .then(db => console.log('db conectada'))
   .catch(err => console.log(err));
 
